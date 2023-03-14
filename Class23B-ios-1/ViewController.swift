df3dfafe0acd27762cfdae98bb7891b3b43e136f
guymacin <guyskv@gmail.com>
2023-03-14 17:45:34 +0200

    Initial Commit

---
+++ Class23B-ios-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class23B-ios-1
+//
+//  Created by Guy Macin on 14/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
